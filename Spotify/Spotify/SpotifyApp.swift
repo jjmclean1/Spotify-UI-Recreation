030919ee2dc9e82c43ec9d68a2bc1f80508fcf5b
jjmclean1 <jjmclean@unc.edu>
2026-02-11 00:20:23 -0500

    Initial Xcode setup

---
+++ Spotify/Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Julian Mclean on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
